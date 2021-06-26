9cf2006be88e5dbdc8b0a27ef223880fa64e2e09
Brayo <vukubrian@gmail.com>
2021-06-26 15:31:00 +0300

    removed tqdm and duplicates

--- setup.py
+++ setup.py
@@ -11,9 +11,7 @@ setup(
     install_requires=[
         "spotipy",
         "pytube",
-        "tqdm",
         "rapidfuzz",
-        "rauth",
         "rich",
         "requests",
         "mutagen",
