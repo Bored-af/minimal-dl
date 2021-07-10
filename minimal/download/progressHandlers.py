040bacb92987a887885736ca502470e8e3466202
brayo <vukubrian@gmail.com>
2021-07-10 16:58:12 +0300

    fixed issue with progressbar

--- minimal/download/progressHandlers.py
+++ minimal/download/progressHandlers.py
@@ -12,7 +12,7 @@
 #! These are not used, they're here for static type checking using mypy
 from typing import List
 from bisect import insort
-from math import ceil
+from math import floor
 from rich.console import Console
 from rich.progress import BarColumn, TimeRemainingColumn, Progress, ProgressColumn
 from rich.progress import Task
@@ -270,7 +270,7 @@ class _ProgressTracker:
         # ! It's scaled to 90 because, the arbitrary division of each songs 100
         # ! iterations is (a) 90 for download (b) 5 for conversion & normalization
         # ! and (c) 5 for ID3 tag embedding
-        iterFraction = int(ceil((len(chunk) / fileSize))) * 90
+        iterFraction = int(floor((len(chunk) / fileSize))) * 90
 
         self.progress += iterFraction
         self.update("Downloading")
